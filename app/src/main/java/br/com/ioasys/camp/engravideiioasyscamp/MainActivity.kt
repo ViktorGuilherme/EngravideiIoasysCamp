d99e69e2a1ebbef4da3f8ac8e02c9e2c1a5c9b61
Viktor Guilherme <viktorguilherme@gmail.com>
2020-02-08 19:06:54 -0300

    Initial commit

---
+++ app/src/main/java/br/com/ioasys/camp/engravideiioasyscamp/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.ioasys.camp.engravideiioasyscamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
