60b8398686877c2e81e5ce37b4ce39dfebb5b4f8
Viktor Guilherme <viktorguilherme@gmail.com>
2020-02-13 16:02:40 -0300

    alterações automaticas

---
+++ app/src/main/java/br/com/ioasys/camp/engravideiioasyscamp/activity/Home.kt
@@ -0,0 +1,13 @@
+package br.com.ioasys.camp.engravideiioasyscamp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.ioasys.camp.engravideiioasyscamp.R
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
