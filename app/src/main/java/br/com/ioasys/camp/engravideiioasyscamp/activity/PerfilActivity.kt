7f07b5c88ad86992d315a98c87e4d280ef7b5a29
Viktor Guilherme <viktorguilherme@gmail.com>
2020-02-14 09:57:04 -0300

    [+] SharedPreferences nos dados da tela perfil

--- app/src/main/java/br/com/ioasys/camp/engravideiioasyscamp/activity/PerfilActivity.kt
+++ app/src/main/java/br/com/ioasys/camp/engravideiioasyscamp/activity/PerfilActivity.kt
@@ -1,14 +1,29 @@
 package br.com.ioasys.camp.engravideiioasyscamp.activity
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.ioasys.camp.engravideiioasyscamp.R
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_perfil.*
 
 class PerfilActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+
+        val prefs = getSharedPreferences("REGIST", Context.MODE_PRIVATE)
+        val nome = prefs.getString("NOME", "")
+        val email = prefs.getString("EMAIL", "")
+        val dataNasc = prefs.getString("DATANASC","")
+        val dataParto = prefs.getString("DATAPARTO","")
+
+
+        campoNome.text = nome
+        campoDataNascimento.text = dataNasc
+        campoParto.text = dataParto
+        campoEmail.text = email
 
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setHomeButtonEnabled(true)
